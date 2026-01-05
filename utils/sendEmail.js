10b8f6df6e764bbfbb057dcc75f4f8b9f21bf9ac
merntest1-ux <merntest1@gmail.com>
2026-01-06 07:18:20 +0800

    Update sendEmail.js

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -13,13 +13,17 @@ async function sendEmail({ to, subject, text }) {
     }
 
     const transporter = nodemailer.createTransport({
-      service: 'gmail',
+      host: 'smtp.gmail.com',
+      port: 587,
+      secure: false, // Use TLS
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
-      // Add timeout settings
-      connectionTimeout: 10000, // 10 seconds
+      tls: {
+        rejectUnauthorized: false
+      },
+      connectionTimeout: 10000,
       greetingTimeout: 10000,
       socketTimeout: 10000,
     });
@@ -74,3 +78,4 @@ async function sendEmail({ to, subject, text }) {
 }
 
 module.exports = sendEmail;
+
